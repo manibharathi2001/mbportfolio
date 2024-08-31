629bdab0d92b5f31dcfedc8f80a773b2d6ea7125
Mani bharathi <128881256+manibharathi2001@users.noreply.github.com>
2024-09-01 00:36:39 +0530

    Add files via upload

---
+++ send-mail.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Email recipient
+    $to = "manibharathi8005@gmail.com"; 
+    $email_subject = "Contact Form Submission: " . $subject;
+    $headers = "From: " . $email . "\r\n" .
+               "Reply-To: " . $email . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    // Email content
+    $email_body = "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Subject: $subject\n\n";
+    $email_body .= "Message:\n$message";
+
+    // Send email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
